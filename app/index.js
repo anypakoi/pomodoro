b25237f5efb6d31e7a124dabb06bbaa83b3ce11b
anypakoi <lucademi16@gmail.com>
2024-11-07 13:02:31 -0300

    slide

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput,} from 'react-native';
+import Slider from '@react-native-community/slider'
 import Countdown from '../components/Countdown';
 import Alarm from '../components/Alarm';
 
@@ -10,7 +11,7 @@ class Counter extends React.Component {
       totalTime: (props.workTime || 25) * 60,
       pomodoro: true,
       playAlarm: false,
-      cycle: 0,
+      cycle: 4,
     };
     this.interval = null;
   }
@@ -38,7 +39,8 @@ class Counter extends React.Component {
     } 
     else {
       this.setState(prevState => ({
-        totalTime: this.state.pomodoro ? this.props.breakTime * 60 : this.props.workTime * 60,
+        
+        totalTime: this.state.pomodoro ? (this.state.cycle % 4 === 0 ? this.props.breakTime * this.props.timeCycle * 60 : this.props.breakTime * 60) : this.props.workTime * 60,
         pomodoro: !prevState.pomodoro,
         cycle: prevState.cycle + 1,
       }))
@@ -61,7 +63,8 @@ export default class App extends React.Component {
     this.state = {
       start: false,
       workTime: '25',
-      breakTime: '5'
+      breakTime: '5',
+      timeCycle: '3',
     }
   }
 
@@ -74,7 +77,11 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           <Button title='stop' onPress={this.onButtonPress} />
-          <Counter workTime={parseInt(this.state.workTime)} breakTime={parseInt(this.state.breakTime)} />
+          <Counter 
+            workTime={parseInt(this.state.workTime)} 
+            breakTime={parseInt(this.state.breakTime)}
+            timeCycle={parseInt(this.state.timeCycle)} 
+          />
         </View>
       )
     } else {
@@ -96,6 +103,18 @@ export default class App extends React.Component {
               keyboardType="numeric"
               value={this.state.breakTime}
               onChangeText={(text) => this.setState({breakTime: text})}
+            />
+          </View>
+          <View style={styles.sliderContainer}>
+            <Text>Multiplicador de ciclo: {this.state.timeCycle}x</Text>
+            <Text>({parseInt(this.state.timeCycle) * parseInt(this.state.breakTime)} minutos de descanso)</Text>
+            <Slider
+              style={styles.slider}
+              minimumValue={1}
+              maximumValue={5}
+              step={1}
+              value={parseInt(this.state.timeCycle)}
+              onValueChange={(value) => this.setState({timeCycle: value.toString()})}
             />
           </View>
           <Button title='start' onPress={this.onButtonPress} />
@@ -124,5 +143,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 5,
     borderRadius: 5,
+  },
+  sliderContainer: {
+    width: '80%',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  slider: {
+    width: '100%',
+    height: 40,
   },
 }); 
