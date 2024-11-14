387ad47a804d16001f272124c7bf09a576596b69
luca <lucademi16@gmail.com>
2024-11-13 23:25:24 -0300

    Revert "pomodoro fork"
    This reverts commit b640218f9c4b3c8a121a36f4d48386f05d5929c5.

--- components/Countdown.js
+++ components/Countdown.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
@@ -5,23 +6,10 @@ const Countdown = (props) => {
   const minutes = Math.floor(props.totalTime / 60);
   const seconds = props.totalTime % 60;
   
-  // Formato condicional basado en el tiempo total
-  let displayTime;
-  if (minutes === 0) {
-    // Menos de un minuto: mostrar solo segundos (00)
-    displayTime = seconds < 10 ? `0${seconds}` : `${seconds}`;
-  } else if (minutes < 10) {
-    // Entre 1-9 minutos: mostrar como (0:00)
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-    displayTime = `${minutes}:${formattedSeconds}`;
-  } else {
-    // 10 minutos o más: mostrar como (00:00)
-    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-    displayTime = `${formattedMinutes}:${formattedSeconds}`;
-  }
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
-  return <Text>{displayTime}</Text>
+  return <Text>{formattedMinutes}:{formattedSeconds}</Text>
 };
 
-export default Countdown;
+export default Countdown
